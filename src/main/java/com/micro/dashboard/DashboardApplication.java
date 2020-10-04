0fab9fc9b9bb873e776a5f8b6aadf92ee08f6bcf
Hugo <hugoonetto@gmail.com>
2020-10-04 20:26:21 +0200

    dashboard configured to see statistics

---
+++ src/main/java/com/micro/dashboard/DashboardApplication.java
@@ -0,0 +1,19 @@
+package com.micro.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@EnableEurekaClient
+public class DashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardApplication.class, args);
+	}
+
+}
